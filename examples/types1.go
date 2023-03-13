fe0d176ffc76403fb5e3225c26926674bba60788
Filip Karlo Došilović <filipk.dosilovic@gmail.com>
2023-03-13 19:29:31 +0100

    Add an example about type declarations and methods.

---
+++ examples/types1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func main() {
+	boilingFreezingDiffF := (BoilingC - FreezingC).ToFahrenheit()
+	fmt.Printf("%s\n", boilingFreezingDiffF.String())
+}
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+
+func (c Celsius) ToFahrenheit() Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func (f Fahrenheit) ToCelsius() Celsius    { return Celsius((f - 32) * 5 / 9) }
